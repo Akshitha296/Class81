33aa904befb0e34fa7bc7d6a4948b83ff408ea52
Akshitha296 <70026624+Akshitha296@users.noreply.github.com>
2021-01-29 20:56:10 -0500

    Add files via upload

---
+++ components/AppDrawerNavigator.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, FlatList, KeyboardAvoidingView, Alert, Image, Modal, ScrollView } from 'react-native';
+import MyHeader from '../components/myHeader'
+import db from '../config'
+import firebase from 'firebase'
+import { createDrawerNavigator } from 'react-navigation-drawer'
+import {AppTabNavigator} from '../screens/AppTabNavigator'
+import CustomSideBarMenu from './CustomSideBarMenu'
+import SettingsScreen from '../screens/SettingsScreen'
+
+export const AppDrawerNavigator = createDrawerNavigator({
+    Home: {
+        screen: AppTabNavigator
+    }, 
+
+    Settings: {
+        screen: SettingsScreen
+    }
+    }, 
+    {
+        contentComponent: CustomSideBarMenu
+    }, 
+    {
+        initialRouteName: 'Home'
+    }
+)
